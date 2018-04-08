58e0bdc2c2d28e0464065b752ee30c1f5fe2af59
bfusion <barbara.fusinska@gmail.com>
2018-04-08 22:35:02 +0100

    Added interval aggrenations to the Weekdys.R code

--- Weekdays.R
+++ Weekdays.R
@@ -3,5 +3,7 @@ noMissingValues
 noMissingValues$weekday <- factor(ifelse(weekdays(noMissingValues$date) %in% c("Saturday", "Sunday"),
                            "weekend", "weekday"))
 
+intervalsWeekSteps <- aggregate(steps ~ interval + weekday, noMissingValues, FUN=mean)
+
 library(ggplot2)
-ggplot(noMissingValues, aes(interval, steps)) + geom_line() + facet_wrap(~weekday)
+ggplot(intervalsWeekSteps, aes(interval, steps)) + geom_line() + facet_wrap(~weekday)
